1a0605e9aa9d81331234f212be59ae5adfdc6878
lishaochen <lishaochen>
2023-03-22 23:36:39 +0800

    edit 1.js

--- 1.js
+++ 1.js
@@ -2,8 +2,9 @@
  * @Author: lishaochen lishaochen
  * @Date: 2023-03-22 23:28:17
  * @LastEditors: lishaochen lishaochen
- * @LastEditTime: 2023-03-22 23:28:22
+ * @LastEditTime: 2023-03-22 23:36:13
  * @FilePath: /code/1.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 ssss
+edit
